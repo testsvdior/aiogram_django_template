fe39ba01518e97d6c2e656c36e6055dec276ca29
timur <waygrozny95@gmail.com>
2021-12-18 21:42:10 +0300

    Initial commit

---
+++ backend/telegram/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from telegram.models import TelegramUser
+
+
+@admin.register(TelegramUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'user_id', 'username', 'first_name', 'last_name', 
+        'language_code', 'deep_link',
+        'created_at', 'updated_at', "is_blocked_bot",
+    ]
+    list_filter = ["is_blocked_bot", "is_moderator"]
+    search_fields = ('username', 'user_id')
---
+++ backend/telegram/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class TelegramUser(models.Model):
+    # fields copied from https://github.com/itisnotyourenv/django-telegram-bot/blob/main/utils/models.py
+    user_id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=32, null=True, blank=True)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256, null=True, blank=True)
+    language_code = models.CharField(max_length=8, help_text="Telegram client's lang", null=True, blank=True)
+    deep_link = models.CharField(max_length=64, null=True, blank=True)
+
+    is_blocked_bot = models.BooleanField(default=False)
+    is_banned = models.BooleanField(default=False)
+
+    is_admin = models.BooleanField(default=False)
+    is_moderator = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'telegram_users'
+        ordering = ('-created_at',)
+        verbose_name = 'TG user'
+        verbose_name_plural = 'TG users'
