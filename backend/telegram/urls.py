df04b0ef6036e96eda335566e016b1166ae83752
itisnotyourenv <waygrozny95@gmail.com>
2022-02-17 01:32:25 +0300

    Refactoring: using ListCreateAPIView instead of two Views and correctly endpoint name for create tg user.

--- backend/telegram/urls.py
+++ backend/telegram/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from telegram import views
 
 urlpatterns = [
-    path('user/', views.TelegramUserCreateAPIView.as_view(), name='create-user'),
+    path('users/', views.TelegramUserListCreateAPIView.as_view(), name='list-create-user'),
     path('user/<int:pk>', views.TelegramUserGetAPIView.as_view(), name='user-detail'),
-    path('users/', views.TelegramUsersListAPIView.as_view(), name='users-list'),
 ]
