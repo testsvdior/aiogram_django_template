3ef9375d861ecdfabaf16106039abbad3feb2c25
itisnotyourenv <waygrozny95@gmail.com>
2022-03-10 20:47:16 +0300

    feature: /help command.

--- telegram_bot/loader.py
+++ telegram_bot/loader.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from auth import AuthBackend
@@ -11,3 +11,10 @@ storage = MemoryStorage()
 # Initialize bot and dispatcher
 bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
 dp = Dispatcher(bot, storage=storage)
+
+bot_commands = [
+    types.BotCommand(command="/start", description="Register user."),
+    types.BotCommand(command="/users", description="Show bot users."),
+    types.BotCommand(command="/message", description="Send messages to all users."),
+    types.BotCommand(command="/help", description="Help command."),
+]
