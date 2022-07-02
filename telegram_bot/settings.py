c13808499c875909282e1ff3ea40d8d57cff6e96
itisnotyourenv <waygrozny95@gmail.com>
2022-07-02 17:05:38 +0300

    DAB-5: updated compose containers.

--- telegram_bot/settings.py
+++ telegram_bot/settings.py
@@ -1,9 +1,15 @@
+import os
 from pathlib import Path
 from typing import List
 
 import environ
 
-BOT_DIR = Path(__file__).resolve().parent
+IS_DOCKER = os.environ.get("IS_DOCKER", False)
+
+if IS_DOCKER:
+    BOT_DIR = Path(__file__).resolve().parent
+else:
+    BOT_DIR = Path(__file__).resolve().parent.parent
 
 
 # Initialize environ and get environments from .env file
@@ -16,12 +22,12 @@ environ.Env.read_env(BOT_DIR / '.env')
 BOT_TOKEN: str = env('BOT_TOKEN')
 ADMIN_LIST: List = env.list('ADMIN_LIST')
 
-# if we run with Docker we user env BACKEND_URL from docker-compose, else use default address.
-BACKEND_URL: str = env('BACKEND_URL', default='http://django:8000/')
+default_backend_value = "http://django:8000/" if IS_DOCKER else "http://localhost:8000/"
+BACKEND_URL: str = env('BACKEND_URL', default=default_backend_value)
 
 # authentication credentials
-REQUEST_USER_LOGIN: str = env('REQUEST_USER_LOGIN')
-REQUEST_USER_PASSWORD: str = env('REQUEST_USER_PASSWORD')
+REQUEST_USER_LOGIN: str = env('ADMIN_USERNAME')
+REQUEST_USER_PASSWORD: str = env('ADMIN_PASSWORD')
 
 # JWT settings
 ACCESS_TOKEN_LIFETIME: int = env.int('ACCESS_TOKEN_LIFETIME') - 1
