be15ff3804c19f16f937a29b76f75eb9122953f9
itisnotyourenv <waygrozny95@gmail.com>
2022-08-05 12:00:13 +0300

    fixBug/redis - setup Redis server with password.

--- redis/init.setup.sh
+++ redis/init.setup.sh
@@ -4,4 +4,6 @@ echo "Start redis entrypoint"
 
 sudo chmod -R a+wxr /usr/local/redis/data
 
-redis-server /usr/local/etc/redis.conf
+##redis-server --requirepass $REDIS_PASSWORD --port $REDIS_PORT --bind $REDIS_HOST --dir /usr/local/redis/data --daemonize yes
+#redis-server
+redis-server /usr/local/etc/redis.conf --requirepass $REDIS_PASSWORD
--- telegram_bot/loader.py
+++ telegram_bot/loader.py
@@ -2,11 +2,11 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 from auth import AuthBackend
-from settings import BOT_TOKEN, REDIS_HOST, REDIS_PORT
+from settings import BOT_TOKEN, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
 
 auth = AuthBackend()
 
-storage = RedisStorage2(host=REDIS_HOST, port=REDIS_PORT)
+storage = RedisStorage2(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 
 # Initialize bot and dispatcher
 bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
